f795c818cbf12bf0b852cf8be82d630615d29a9a
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-11-28 19:39:49 +0100

    conect

--- client.py
+++ client.py
@@ -5,27 +5,46 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
+
+
+
+print("\r\n")
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try:
+    METODO = sys.argv[1]
+    receptor = sys.argv[2] #batman@193.147.73.20:5555
+    primer = receptor.split("@")
+    LOGIN = primer[0]
+    resto = primer[1].split(":")
+    IP = resto[0]
+    PUERTO = resto[1]
+except:
+    print("Usage: python3 client.py method receiver@IP:SIPport")
+print(METODO + LOGIN + IP + PUERTO)
+#login = separador.split(" ")[1]
+
+#SERVER = 'localhost'
+#PORT = 6001
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+#LINE = '¡Hola mundo!'
+LINE = ("INVITE sip:" + LOGIN + "@" + IP + " SIP/2.0\r\n")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, int(PUERTO)))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
+#print("Terminando socket...")
 
 # Cerramos todo
 my_socket.close()
