e729d947996d852e819551938f7b08cfb2b58306
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-11-29 19:33:04 +0100

    fallo32

--- client.py
+++ client.py
@@ -7,16 +7,9 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-
-
-print("\r\n")
-
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
 try:
     METODO = sys.argv[1]
-    receptor = sys.argv[2] #batman@193.147.73.20:5555
+    receptor = sys.argv[2]
     primer = receptor.split("@")
     LOGIN = primer[0]
     resto = primer[1].split(":")
@@ -24,14 +17,7 @@ try:
     PUERTO = resto[1]
 except:
     print("Usage: python3 client.py method receiver@IP:SIPport")
-#print(METODO + LOGIN + IP + PUERTO)
-#login = separador.split(" ")[1]
 
-#SERVER = 'localhost'
-#PORT = 6001
-
-# Contenido que vamos a enviar
-#LINE = '¡Hola mundo!'
 LINE = ("INVITE sip:" + LOGIN + "@" + IP + " SIP/2.0\r\n")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
@@ -39,34 +25,31 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, int(PUERTO)))
 
-#print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 i = 0
 while i < 1:
     data = my_socket.recv(1024)
     respuesta = data.decode('utf-8')
-    print('Recibido -- ' + respuesta)
+    #print('Recibido -- ' + respuesta)
     serv_resp = respuesta.split(" ")
     #print(serv_resp)
     if serv_resp[1] == "200":
-        print("recv 200 ok")
-        ack = "ACK sip:" + LOGIN + "@" + IP + " SIP/2.0\r\n"
-        my_socket.send(bytes(ack, 'utf-8') + b'\r\n')
-        print("enviado el ack")
-    elif serv_resp[1] == "405":
-        print("error 405")
+        print("cerrando cliente")#hemos enviado BYE, responde OK
         i = 2
-    elif serv_resp[1] == "100":
-        print("100 Trying")
+    elif serv_resp[1] == "405":
+        print(respuesta)
+        i = 2
+    elif serv_resp[1] == "100":#trying
+        print(respuesta)
         findok = respuesta.split("\r\n\r\n")
         print(findok)
         okmsg = findok[2].split(" ")
-        if okmsg[2] == "OK":
+        if okmsg[2] == "OK":#3 mns concatenados Trying;Ring;OK
             ack = "ACK sip:" + LOGIN + "@" + IP + " SIP/2.0\r\n"
             my_socket.send(bytes(ack, 'utf-8') + b'\r\n')
             print("enviado el ack")
-    elif serv_resp[1] == "180":
-        print("180 Ring")
+    elif serv_resp[1] == "180":#No deberia pasar por aqui
+        print(respuesta)
 #print("Terminando socket...")
 # Cerramos todo
 my_socket.close()
