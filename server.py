9c3020d8a112f4458c4db1879357cb99350c5be2
diego andres sanchez molina <diegoas@zeta40.aulas.gsyc.es>
2015-11-30 10:55:02 +0100

    funciona

--- server.py
+++ server.py
@@ -13,25 +13,34 @@ class SIPHandler(socketserver.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             IP_Client = str(self.client_address[0])
-            lineutf = line.decode('utf-8')
-            method = lineutf.split(" sip:")
-            metodos = ["INVITE","BYE","ACK"]
-            if method[0] in metodos:
-                if method[0]=="INVITE":
-                    self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
-                    self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
-                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                elif method[0]=="ACK":
-                    os.system("chmod +x mp32rtp")
-                    exe = "./mp32rtp -i " + IP_Client
-                    exe = exe + " -p 23032 < " + audio_file
-                    os.system(exe)
-                elif method[0]=="BYE":
-                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                else:
-                    self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
+            try:
+                lineutf = line.decode('utf-8')
+            except UnicodeDecodeError:
+                lineutf = ""
+            if lineutf != "":
+                #print("entra")
+                method = lineutf.split(" sip:")
+                metodos = ["INVITE","BYE","ACK"]
+                if method[0] in metodos:
+                    if method[0]=="INVITE":
+                        self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                        self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
+                        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                        print(">TryingRingOK")
+                    elif method[0]=="ACK":
+                        os.system("chmod +x mp32rtp")
+                        exe = "./mp32rtp -i " + IP_Client
+                        exe = exe + " -p 23032 < " + audio_file
+                        os.system(exe)
+                        print(">audio_file enviado")
+                    elif method[0]=="BYE":
+                        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                        print(">OK")
+                    else:
+                        self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                        print(">405")
+                    # Si no hay más líneas salimos del bucle infinito
+            else:
                 break
 
 if __name__ == "__main__":
