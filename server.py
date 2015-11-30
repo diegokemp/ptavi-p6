8b97939dda69bdbcc111da6760db1f8e981bdd75
diego andres sanchez molina <diegoas@zeta40.aulas.gsyc.es>
2015-11-30 11:11:43 +0100

    finalizando

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ import os
 
 class SIPHandler(socketserver.DatagramRequestHandler):
 
-
     def handle(self):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -20,25 +19,25 @@ class SIPHandler(socketserver.DatagramRequestHandler):
             if lineutf != "":
                 #print("entra")
                 method = lineutf.split(" sip:")
-                metodos = ["INVITE","BYE","ACK"]
+                metodos = ["INVITE", "BYE", "ACK"]
                 if method[0] in metodos:
-                    if method[0]=="INVITE":
+                    if method[0] == "INVITE":
                         self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
                         self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
                         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                         print(">TryingRingOK")
-                    elif method[0]=="ACK":
+                    elif method[0] == "ACK":
                         os.system("chmod +x mp32rtp")
                         exe = "./mp32rtp -i " + IP_Client
                         exe = exe + " -p 23032 < " + audio_file
                         os.system(exe)
                         print(">audio_file enviado")
-                    elif method[0]=="BYE":
+                    elif method[0] == "BYE":
                         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                         print(">OK")
-                    else:
-                        self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-                        print(">405")
+                else:
+                    self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                    print(">405")
                     # Si no hay más líneas salimos del bucle infinito
             else:
                 break
